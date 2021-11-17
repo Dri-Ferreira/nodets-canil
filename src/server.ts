5db02b470c591e99dd8620a43f33a465f7796858
Adriano Hilario Ferreira <adrianoferreira31@gmail.com>
2021-11-16 21:01:58 -0300

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res)=>{ // aviso de error se nenhuma página for encontrada
-    res.send('Página não encontrada!')
+    res.render('pages/404');
 });
 
 
